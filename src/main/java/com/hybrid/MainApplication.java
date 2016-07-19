50bdbf731fec316fa9e155f50f342dd3a2dbdaa1
java <java>
2016-07-19 15:24:00 +0900

    10ea Hello

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,7 +4,9 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		System.out.println("##################");
+		for(int i=0; i<10; i++) {
 		System.out.println("HEllo First Gradle");
+		}
 		System.out.println("##################");
 	}
 }
