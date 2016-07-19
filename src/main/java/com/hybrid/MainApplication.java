2d1ca86c533999997795bb432386eb5ec7245951
java <java>
2016-07-19 14:32:48 +0900

    first commit

---
+++ src/main/java/com/hybrid/MainApplication.java
@@ -0,0 +1,10 @@
+package com.hybrid;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("##################");
+		System.out.println("HEllo First Gradle");
+		System.out.println("##################");
+	}
+}
---
+++ src/main/java/com/hybrid/Second.java
@@ -0,0 +1,10 @@
+package com.hybrid;
+
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.println("###################");
+		System.out.println("HEllo Second Gradle");
+		System.out.println("###################");
+	}
+}
